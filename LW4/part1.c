ffcc5a44e229019e9abf16d73667fa1a68a09e2d
Stanislau Marudau <marudov.stas@gmail.com>
2023-12-08 12:36:23 +0100

    Added a working c-scan

--- LW4/part1.c
+++ LW4/part1.c
@@ -1,124 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define CYLINDERS 5000
-#define REQUESTS 10
-
-int sstf(int requests[], int initial_position);
-int scan(int requests[], int initial_position);
-int c_scan(int requests[], int initial_position);
-int look(int requests[], int initial_position);
-int c_look(int requests[], int initial_position);
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <initial_position>\n", argv[0]);
-        return 1;
-    }
-
-    //Setup the initial position of the disk head
-    int initial_position = atoi(argv[1]);
-    //Inititalize an array of 1000 requestes
-    int requests[REQUESTS];
-
-    // Generate 1000 random requests
-    for (int i = 0; i < REQUESTS; ++i) {
-        requests[i] = rand() % CYLINDERS;
-        printf('%d \t', requests[i]);
-    }
+#define DISK_SIZE 200
+#define REQUESTS 8
 
 
-    printf("Initial position: %d\n", initial_position);
-    //Run the disk-scheduling algorithms
-    fcfs(requests, initial_position);
-    sstf(requests, initial_position);
-    scan(requests, initial_position);
-    c_scan(requests, initial_position);
-    look(requests, initial_position);
-    c_look(requests, initial_position);
-
-    printf("Total number of head movements made by:\n");
-    printf("FCFS: %d", fcfs(requests, initial_position));
-    printf("SSTF: %d", fcfs(requests, initial_position));
-    printf("SCAN: %d", fcfs(requests, initial_position));
-    printf("C-SCAN: %d", fcfs(requests, initial_position));
-    printf("LOOK: %d", fcfs(requests, initial_position));
-    printf("C-LOOK: %d", fcfs(requests, initial_position));
-    return 0;
+// Function to compare two integers for qsort
+int cmpfunc(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
 }
 
-int fcfs(int requests[], int initial_position) {
-    //Initial number of head movements
+int cscan(int requests[], int head)
+{
     int head_movement = 0;
-    int current_position = initial_position;
+    int current_position = head;
+    qsort(requests, REQUESTS, sizeof(int), cmpfunc);
+    // Find the index where the current position is located
+    int count = 0;
+    for (int j = 0; j < REQUESTS; j++)
+    {
+        if (requests[j] < current_position)
+        {
+            count++;
+        }
+    }
 
-    for (int i = 0; i < REQUESTS; ++i) {
-        head_movement += abs(requests[i] - current_position);
-        current_position = requests[i];
+    // Initialize two arrays for left and right requests
+    int left[count];
+    int right[REQUESTS - count];
+
+    // Fill the left array with smaller requests
+    for (int i = 0; i < count; ++i)
+    {
+        left[i] = requests[i];
+    }
+
+    // Fill the right array with greater requests
+    for (int i = count; i < REQUESTS; ++i)
+    {
+        right[i - count] = requests[i];
+    }
+
+    // Process requests in the forward direction for the right array
+    for (int i = 0; i < REQUESTS - count; ++i)
+    {
+        head_movement += abs(right[i] - current_position);
+        current_position = right[i];
+    }
+    // Go to the start of the disk and set the current position as 0
+
+    head_movement += abs(DISK_SIZE - current_position - 1);
+    head_movement += DISK_SIZE - 1;
+
+    current_position = 0;
+    // Process requests in the left array
+    for (int i = 0; i < count; i++)
+    {
+        head_movement += abs(left[i] - current_position);
+        current_position = left[i];
     }
 
     return head_movement;
 }
 
-void quicksort(int array[REQUESTS],int low,int high) {
+int main(int argc, char *argv[])
+{
+    int requests[REQUESTS] = { 176, 79, 34, 60, 92, 11, 41, 114 };
+    int initial_head;
 
-int x, y, p, temp;
-
-if(low<high) {
-
-    p=low;
-    x=low;
-    y=high;
-
-    while(x<y){
-        while(array[x]<=array[p]&&x<high)
-        x++;
-            while(array[y]>array[p])
-            y--;
-            if(x<y){
-                temp=array[x];
-                array[x]=array[y];
-                array[y]=temp;
-            }
-        }
-        temp=array[p];
-        array[p]=array[y];
-        array[y]=temp;
-
-        quicksort(array,low,y-1);
-        quicksort(array,y+1,high);
+    // Check for valid command line argument for initial head position
+    if (argc != 2)
+    {
+        printf("Usage: %s <initial_head_position>\n", argv[0]);
+        return -1;
     }
-}
 
-int sstf(int requests[], int initial_position) {
-    //Initial number of head movements
-    int head_movement = 0;
-    int current_position = initial_position;
-    for(int i =0; i < REQUESTS; i++){
-
+    initial_head = atoi(argv[1]);
+    if (initial_head < 0 || initial_head >= DISK_SIZE)
+    {
+        printf("Invalid initial head position. Must be between 0 and %d.\n", DISK_SIZE - 1);
+        return -1;
     }
-}
 
-int scan(int requests[], int initial_position) {
-    //Initial number of head movements
-    int head_movement = 0;
-    int current_position = initial_position;
-}
+    qsort(requests,REQUESTS, sizeof(int), cmpfunc);
 
-int c_scan(int requests[], int initial_position) {
-    //Initial number of head movements
-    int head_movement = 0;
-    int current_position = initial_position;
-}
+    // Seed random number generator
+    srand(time(NULL));
 
-int look(int requests[], int initial_position) {
-    //Initial number of head movements
-    int head_movement = 0;
-    int current_position = initial_position;
-}
+    // Apply and print results of each algorithm
+    printf("Total head movement for C-SCAN: %d\n", cscan(requests, initial_head));
+    // Call and print results for other algorithms
 
-int c_look(int requests[], int initial_position) {
-    //Initial number of head movements
-    int head_movement = 0;
-    int current_position = initial_position;
+    return 0;
 }
